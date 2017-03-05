fc495364b97537d944b695efe3580c340fdc0b27
Evan Martinez <evanm.design@gmail.com>
2017-03-05 10:30:53 -0500

    got the cardboard icon to show up

--- app/components/vr-box.js
+++ app/components/vr-box.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  attributeBindings: ['color', 'depth', 'opacity'],
+  attributeBindings: ['color', 'position'],
   color: '#6173F4',
-  depth: 2,
-  opacity: '0.8',
+  position: Ember.computed('positionX', 'positionY', 'positionZ', function() {
+    return `${this.get('positionX')} ${this.get('positionY')} ${this.get('positionZ')}`;
+  }),
+  positionX: 0,
+  positionY: 2,
+  positionZ: -5,
   tagName: 'a-box',
 });
